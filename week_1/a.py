48c7ff99d028457f3ae3bf31525d9c27f6e70206
Amruta190 <61353921+Amruta190@users.noreply.github.com>
2021-01-05 21:22:52 +0530

    My Python subject Programs

---
+++ week_1/a.py
@@ -0,0 +1,7 @@
+look= int(input('Enter a number'))
+if look==1:
+    print("Python")
+elif look==2:
+    print("Day-1")
+else:
+    print("Input should be either 1 or 2")
---
+++ week_1/d.py
@@ -0,0 +1,4 @@
+sum=0
+for i in range(2,51,2):
+    sum+=i
+print(f"Sum of even numbers from 1 to 50 is {sum}")
